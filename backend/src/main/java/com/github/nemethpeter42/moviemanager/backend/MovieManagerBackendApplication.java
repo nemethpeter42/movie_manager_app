49e89e8b94e2b85b30d6c13663df285017f9e646
unknown <nemeth.peter.42@gmail.com>
2023-08-15 17:41:51 +0200

    elso commit

---
+++ backend/src/main/java/com/github/nemethpeter42/moviemanager/backend/MovieManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.nemethpeter42.moviemanager.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieManagerBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/github/nemethpeter42/moviemanager/backend/MovieManagerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nemethpeter42.moviemanager.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieManagerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
