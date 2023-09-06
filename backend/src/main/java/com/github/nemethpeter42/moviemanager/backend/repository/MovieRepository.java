adebd7afff4127d51d8d8a0a2684cd8504f503fc
unknown <nemeth.peter.42@gmail.com>
2023-09-06 12:37:43 +0200

    @Repostiory eltavolitasa hint miatt, tailwind felrakasa

--- backend/src/main/java/com/github/nemethpeter42/moviemanager/backend/repository/MovieRepository.java
+++ backend/src/main/java/com/github/nemethpeter42/moviemanager/backend/repository/MovieRepository.java
@@ -5,11 +5,9 @@ import com.github.nemethpeter42.moviemanager.backend.model.Movie;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
 public interface MovieRepository extends MongoRepository<Movie, String> {
 
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,21 +15,46 @@ function App() {
     <div className="App">
       {" "}
       <div className="addMovie">
-        <input
-          type="text"
-          placeholder="Original title..."
-          onChange={(event) => {
-            setOriginalTitle(event.target.value);
-          }}
-        />
-        <input
-          type="text"
-          placeholder="Local title..."
-          onChange={(event) => {
-            setLocalTitle(event.target.value);
-          }}
-        />
+        <div>
+          <input 
+            type="text" 
+            id="originalTitle" 
+            className="
+              border text-sm rounded-lg block w-full p-2.5 m-1
+              text-gray-900 bg-gray-50 border-gray-300 
+              focus:ring-blue-500 focus:border-blue-500 
+              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+              dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Original title..." 
+            onChange={(event) => {
+              setOriginalTitle(event.target.value);
+            }}
+            />
+        </div>
+        <div>
+          <input 
+            type="text" 
+            id="localTitle" 
+            className="
+              border text-sm rounded-lg block w-full p-2.5 m-1
+              text-gray-900 bg-gray-50 border-gray-300 
+              focus:ring-blue-500 focus:border-blue-500 
+              dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+              dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+            placeholder="Local title..." 
+            onChange={(event) => {
+              setLocalTitle(event.target.value);
+            }}
+            />
+        </div>
         <button
+        className="
+          font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 
+          text-white bg-blue-700 
+          hover:bg-blue-800 
+          focus:ring-4 focus:ring-blue-300 focus:outline-none
+          dark:bg-blue-600 dark:hover:bg-blue-700 
+          dark:focus:ring-blue-800"
           onClick={() => {
             dispatch(
               addMovie({
@@ -45,10 +70,10 @@ function App() {
         </button>
       </div>
       <div className="displayMovies">
-        {movieList.map((movie) => {
+        {movieList.map((movie,i) => {
           return (
-            <div>
-              <h1> {movie.originalTitle}</h1>
+            <div key={i}>
+              <h1 className="underline text-gray-500"> {movie.originalTitle}</h1>
               <h1> {movie.localTitle}</h1>
               <input
                 type="text"
