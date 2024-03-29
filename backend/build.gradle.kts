48da1500306156a5025a920aa06968cc810a4303
Peter Nemeth <nemeth.peter.42@gmail.com>
2024-03-29 09:35:08 +0100

    fixed gradle lombok version

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -26,8 +26,8 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation ("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.2.0")
 	implementation ("org.springframework.boot:spring-boot-starter-actuator")
-	compileOnly("org.projectlombok:lombok")
-	annotationProcessor("org.projectlombok:lombok")
+	compileOnly("org.projectlombok:lombok:1.18.30")
+	annotationProcessor("org.projectlombok:lombok:1.18.30")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
